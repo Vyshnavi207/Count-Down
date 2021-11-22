b2c8abef6affb4d21cecf1493a6ea86a9ec8c30b
kaavin <kaavinak@gmail.com>
2021-11-22 22:57:18 +0530

    random bug fix

--- functions/arraydatefunction.js
+++ functions/arraydatefunction.js
@@ -3,25 +3,25 @@ let arraydates=(dates)=>
     let array=[]
     let sortedarray=[];
 
-    
+   
     dates= dates.filter((item)=>
     {
         if(new Date(moment(item,`YYYY-MM-DDhh:mm:ss.sss`).format('MMM D, YYYY HH:mm:ss')).getTime() > new Date().getTime())
             {
-                console.log(item)
+               
                 return item
       
             }
     })
-    console.log("dates arrya man ",dates)
+   
     for(let i=0;i<dates.length;i++)
     {
      
-    // if(new Date().getTime()<=new Date(tempdate).getTime())
-    // {
+   
+  
       array.push(moment(dates[i],`YYYY-MM-DDhh:mm:ss.sss`).format('MMM D, YYYY HH:mm:ss'))
 
-    // }
+   
 
     }
 
--- functions/starttimer.js
+++ functions/starttimer.js
@@ -1,18 +1,10 @@
 function startthetimer (datenumber){
-  console.log("other info ",datenumber, "okay man  ",datearray)
-  // var currentDate = new Date();
-
-  // var futureDate = new Date(datearray[datenumber]);
-
-  // var difference = futureDate.getTime() /1000 - currentDate.getTime() /1000;
+ 
   
-//  console.log('diff bruh ',futureDate.getTime()-currentDate.getTime())
-//  console.log('date array ',datearray)
-
  
   if(datearray.length==0)
   {
- console.log("okay man ")
+ 
   let div = document.createElement('div')
   div.innerText="NO EVENTS FOR NOW"
   div.id='no-events-id'
