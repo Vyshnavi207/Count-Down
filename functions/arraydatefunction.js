a64bb0e6edcdbf5d7beb0cb52760a1e9597ea666
kaavin <kaavinak@gmail.com>
2021-11-23 03:09:11 +0530

    ascending order arrangment for events in countdown

--- functions/arraydatefunction.js
+++ functions/arraydatefunction.js
@@ -2,8 +2,8 @@ let arraydates=(dates)=>
 {
     let array=[]
     let sortedarray=[];
-
-   
+    
+  
     dates= dates.filter((item)=>
     {
         if(new Date(moment(item,`YYYY-MM-DDhh:mm:ss.sss`).format('MMM D, YYYY HH:mm:ss')).getTime() > new Date().getTime())
@@ -24,10 +24,30 @@ let arraydates=(dates)=>
    
 
     }
+    array=array.map((item,index)=>
+    {
+         return {
+           date:item,
+           title:eventinfoarray[index]['title'],
+           description:eventinfoarray[index]['description']
+         }
+    })
 
     sortedarray=bblSort(array)
 
-
+  
+    eventinfoarray=sortedarray.map((item)=>
+    {
+      return {
+        title:item['title'],
+        description:item['description']
+      }
+    })
+    sortedarray=sortedarray.map((item)=>
+    {
+      return item['date']
+    })
+   
 
     return [...sortedarray]
 }
@@ -40,12 +60,14 @@ function bblSort(arr){
     for(var i = 0; i < arr.length; i++){
    
       for(var j = 0; j < ( arr.length - i -1 ); j++){
-          
-        time1 = new Date(datearray[j]);
-        time2 = new Date(datearray[j+1]);
+         
+        time1 = new Date(arr[j]['date']);
+        time2 = new Date(arr[j+1]['date']);
+      
+       
         if(time1.getTime()> time2.getTime()){
             
-       
+        
           var temp = arr[j]
           arr[j] = arr[j + 1]
           arr[j+1] = temp
