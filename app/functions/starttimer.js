448b0aad8082caa03f85bc1d52ab6ebe3ea7d894
vyshnavi madikonda <vyshnavi.madikonda@gmail.com>
2021-12-19 22:24:43 +0530

    Connected with the backend

---
+++ app/functions/starttimer.js
@@ -0,0 +1,28 @@
+function startthetimer(datenumber, status) {
+  if (datearray.length == 0) {
+    if (status != "backenddown") {
+      let div = document.createElement("div");
+      div.innerText = "NO EVENTS FOR NOW";
+      div.id = "no-events-id";
+      let timerbox = document.querySelector(".timer");
+      timerbox.appendChild(div);
+    }
+    if (status == "backenddown") {
+      let div = document.createElement("div");
+      div.innerText = "BACKEND DOWN SERVER ERROR";
+      div.id = "no-events-id";
+      let timerbox = document.querySelector(".timer");
+      timerbox.appendChild(div);
+    }
+  } else {
+    var currentDate = new Date();
+    var futureDate = new Date(datearray[datenumber]);
+    var difference = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+    titlediv.innerHTML = eventinfoarray[datenumber]["title"];
+    descriptiondiv.innerHTML = eventinfoarray[datenumber]["description"];
+    clock = $(".clock").FlipClock(difference, {
+      clockFace: "DailyCounter",
+      countdown: true,
+    });
+  }
+}
